cfd1483a992e627ecaf8f41edbdd95cb99713609
Ankan Bhaumik <ankanbhaumik80@gmail.com>
2022-06-10 14:36:45 +0530

    2

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,13 @@ import * as geometry from 'spherical-geometry-js';
 
 export default function Home() {
   const[distance,setDistance] =useState(0);
+  const[l,seL]=useState(0);
+  const[l2,seL2]=useState(0);
   useEffect(()=>{
     navigator.geolocation.watchPosition(function(position) {
       console.log(position.coords.latitude,position.coords.longitude);
+      seL(position.coords.latitude)
+      seL2(position.coords.longitude)
       //getDistance(27.0093299,88.2169027,position.coords.latitude,position.coords.longitude);
       var x=geometry.computeDistanceBetween(new geometry.LatLng(27.0093299,88.2169027),new geometry.LatLng(position.coords.latitude,position.coords.longitude));
       setDistance(x);
@@ -21,6 +25,8 @@ export default function Home() {
   return (
     <div >
       <h1>{distance} meters</h1>
+      <p>latitude= {l}</p>
+      <p>longitude= {l2}</p>
     </div>
   )
 }
