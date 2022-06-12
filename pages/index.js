9c6c0c87ab1e72b1beb019b1fa81b3359bb8c736
Ankan Bhaumik <ankanbhaumik80@gmail.com>
2022-06-12 17:48:44 +0530

    b

--- pages/index.js
+++ pages/index.js
@@ -16,15 +16,22 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <meta property="og:title" content="The Rock" />
-        <meta property="og:type" content="video.movie" />
         <meta
           property="og:url"
-          content="https://www.imdb.com/title/tt0117500/"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
         />
         <meta
           property="og:image"
-          content="https://ia.media-imdb.com/images/rock.jpg"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
         />
       </Head>
       <input
